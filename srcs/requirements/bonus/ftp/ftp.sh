40eacfcf064f19c53e5770c60502660809d5a759
MAROUAN17 <marinosaglagal25@gmail.com>
2025-05-16 16:07:58 +0100

    ftp change passive mode

--- srcs/requirements/bonus/ftp/ftp.sh
+++ srcs/requirements/bonus/ftp/ftp.sh
@@ -4,7 +4,17 @@ mkdir -p /var/run/vsftpd/empty;
 chown root:root /var/run/vsftpd/empty;
 chmod 755 /var/run/vsftpd/empty;
 
+sed -i 's|listen=NO|listen=YES|' /etc/vsftpd.conf
+sed -i 's|listen_ipv6=YES|listen_ipv6=NO|' /etc/vsftpd.conf
+echo "pasv_enable=YES" >> /etc/vsftpd.conf
+echo "port_enable=YES" >> /etc/vsftpd.conf
+echo "pasv_max_port=10100" >> /etc/vsftpd.conf
+echo "pasv_min_port=10090" >> /etc/vsftpd.conf
+echo "pasv_address=127.0.0.1" >> /etc/vsftpd.conf
+echo "local_root=/var/www/html" >> /etc/vsftpd.conf
+echo "pasv_addr_resolve=YES" >> /etc/vsftpd.conf
+
 useradd -m ${FTP_USER};
 echo "${FTP_USER}:$(cat "${FTP_PASS}")" | chpasswd
 
-vsftpd;
+vsftpd /etc/vsftpd.conf;
