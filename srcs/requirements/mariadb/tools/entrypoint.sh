fcc7bb56a8cd931a5404e725e4ef9227fc858b15
MAROUAN17 <marinosaglagal25@gmail.com>
2025-05-21 14:39:04 +0100

    add password to root user

--- srcs/requirements/mariadb/tools/entrypoint.sh
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
 
-mysqld_safe &
+#mysqld_safe &
+
+mkdir -p /run/mysql
+chown mysql:mysql /run/mysql
+
+mysql_install_db --user=mysql
+
+mysqld_safe & 
 
 sleep 5;
 
-mysql -u root -p$(cat ${MARIADB_DB_ROOT_PASSWORD}) -h 127.0.0.1  <<-EOSQL
+mysql -u root <<-EOSQL
     ALTER USER 'root'@'localhost' IDENTIFIED BY '$(cat ${MARIADB_DB_ROOT_PASSWORD})';
     CREATE DATABASE IF NOT EXISTS ${MARIADB_DB_NAME};
     CREATE USER IF NOT EXISTS '${MARIADB_DB_USER}'@'%' IDENTIFIED BY '$(cat ${WORDPRESS_USER_PASSWORD})';
